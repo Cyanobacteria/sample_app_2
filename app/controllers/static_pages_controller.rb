aa5ae6ad30140715f53f096b69408d67f3d8fd56
cyanobacteria <vodomelkor@gmail.com>
2017-08-13 21:48:02 +0800

    用另外一種方式實做了page name的顯示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,33 @@
 class StaticPagesController < ApplicationController
+  #before_action :set_page_name
   def home
+    name = this_method_name.split('')
+    name.first.upcase!
+    @page_name = name.join
   end
 
   def help
+    name = this_method_name.split('')
+    name.first.upcase!
+    @page_name = name.join
   end
 
   def about
-
+    name = this_method_name.split('')
+    name.first.upcase!
+    @page_name = name.join
   end
+
+
+  private
+   # def set_page_name
+    #  @page_name = this_method_name
+    #end 
+ 
+    def this_method_name
+      caller[0] =~ /`([^']*)'/ and $1
+    end
+
+
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  #方法名子不可以有_?? ex: set_up會錯誤
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 
