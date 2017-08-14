b96bedec43c9b5ccabcf8d8c3344638f7b8d93d4
cyanobacteria <vodomelkor@gmail.com>
2017-08-14 12:19:45 +0800

    重構view標題的顯示方式

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,32 +1,29 @@
 class StaticPagesController < ApplicationController
   #before_action :set_page_name
   def home
-    name = this_method_name.split('')
-    name.first.upcase!
-    @page_name = name.join
   end
 
   def help
-    name = this_method_name.split('')
-    name.first.upcase!
-    @page_name = name.join
   end
 
   def about
-    name = this_method_name.split('')
-    name.first.upcase!
-    @page_name = name.join
   end
 
 
   private
-   # def set_page_name
-    #  @page_name = this_method_name
-    #end 
+=begin 
+    另外一種嘗試，純粹是好玩，不過不夠有效率,所以就收起來做個紀念   
+    def set_page_name
+      @page_name = this_method_name
+    end 
  
     def this_method_name
       caller[0] =~ /`([^']*)'/ and $1
     end
+    name = this_method_name.split('')
+    name.first.upcase!
+    @page_name = name.join
+=end
 
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def title(tar_title = '' )
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if tar_title == nil
+      base_title
+    else
+      final = "#{tar_title} | #{base_title}"
+      final
+    end
+  end
+
+
+
+
+
+
 end
