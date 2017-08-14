08828e0a0c87b0bf67dc103cc67cd7a92ef77483
cyanobacteria <vodomelkor@gmail.com>
2017-08-14 12:27:20 +0800

    去掉首頁的home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def title(tar_title = '' )
     base_title = "Ruby on Rails Tutorial Sample App"
-    if tar_title == nil
+    if tar_title == ''
       base_title
     else
       final = "#{tar_title} | #{base_title}"
