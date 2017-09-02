e93e8901e652bf2d6012475558ff392385e8196c
cyanobacteria <vodomelkor@gmail.com>
2017-09-03 03:01:35 +0800

    重新學一次

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   before_save { self .email = email.downcase }
   #before_save { email.downcase! }
   validates :name, presence: :true, length: { maximum: 50 }
@@ -9,4 +10,5 @@ class User < ApplicationRecord
 
   has_secure_password 
   validates :password, presence: true, length: { minimum: 6 }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+value = [["john", "john@john.john"], ["kiki", "kiki@kiki.kiki"],
+         ["taph", "taph@taph.taph"], ["dan", "dan@dan.dan"], ["joy", "joy@joy.joy"]]
+value.each do |v|
+User.create(:name => "#{v[0]}", :email => "#{v[1]}",
+             password: "aaaaaa", password_confirmation: "aaaaaa" )
+end
